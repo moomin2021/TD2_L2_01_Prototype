f7a5282cfec17f4a4c96b597b9fe3cb2fe6e78b2
tamura_fumiya <k021g1134@g.neec.ac.jp>
2022-10-13 11:39:31 +0900

    正面からの当たり判定で埋め込まれないように

--- Collision.cpp
+++ Collision.cpp
@@ -52,23 +52,47 @@ void Collision::Initialize() {
 
 }
 
-// --更新処理
+// --更新処理-- //
 void Collision::Update() {
-	// --プレイヤーの当たり判定フラグがtrueだったら当たり判定を行う
+	// --プレイヤーの当たり判定フラグがONだったら-- //
 	if (player->GetCollisionFlag() == true) {
+		// --黒と白のプレイヤーの座標と半径の情報を格納-- //
 		Box blackObj = { {player->GetBlackObj().pos.x, player->GetBlackObj().pos.y + Camera::GetScroll()}, player->GetBlackObj().radius, player->GetBlackObj().radius };
 		Box whiteObj = { {player->GetWhiteObj().pos.x, player->GetWhiteObj().pos.y + Camera::GetScroll()}, player->GetWhiteObj().radius, player->GetWhiteObj().radius };
+
+		// --障害物の数分当たり判定をとる-- //
 		for (int i = 0; i < stage->obstacles.size(); i++) {
+			// --障害物の座標と半径を格納-- //
 			Box obstacle = { stage->obstacles[i].GetPos(), stage->obstacles[i].GetRadiusX(), stage->obstacles[i].GetRadiusY() };
+
+			// --白プレイヤーの当たり判定-- //
 			if (BoxCollision(whiteObj, obstacle)) {
+
+				// --当たった障害物が黒色だったら-- //
 				if (stage->obstacles[i].GetColor() == 0x000000) {
-					// --正面から当たっていたら
+					// --正面から当たっていたら-- //
 					if (BoxCenterCol(oldWhiteBox, obstacle)) {
+						// --通常状態なら-- //
 						if (player->GetState() == Normal) {
+							// --プレイヤーと障害物のY軸距離-- //
+							float len = whiteObj.pos.y - obstacle.pos.y;
+
+							// --プレイヤーと障害物のY軸半径を足した値-- //
+							float radius = whiteObj.radiusY + obstacle.radiusY;
+
+							// --カメラに距離と半径を引いた数を加算-- //
+							Camera::AddScroll(radius - len);
+
+							// --状態をノックバック状態に変更-- //
 							player->SetKnock();
+
+							// --当たった障害物を消す-- //
 							stage->obstacles.erase(stage->obstacles.begin() + i);
 						}
+
+						// --状態がブースト状態なら-- //
 						else if (player->GetState() == Boost) {
+							// --当たった障害物を消す-- //
 							stage->obstacles.erase(stage->obstacles.begin() + i);
 						}
 					}
@@ -82,15 +106,34 @@ void Collision::Update() {
 				}
 			}
 
+			// --黒プレイヤーの当たり判定-- //
 			if (BoxCollision(blackObj, obstacle)) {
+
+				// --当たった障害物が黒色だったら-- //
 				if (stage->obstacles[i].GetColor() == 0xFFFFFF) {
-					// --正面から当たっていたら
+					// --正面から当たっていたら-- //
 					if (BoxCenterCol(oldBlackBox, obstacle)) {
+						// --通常状態なら-- //
 						if (player->GetState() == Normal) {
+							// --プレイヤーと障害物のY軸距離-- //
+							float len = blackObj.pos.y - obstacle.pos.y;
+
+							// --プレイヤーと障害物のY軸半径を足した値-- //
+							float radius = blackObj.radiusY + obstacle.radiusY;
+
+							// --カメラに距離と半径を引いた数を加算-- //
+							Camera::AddScroll(radius - len);
+
+							// --状態をノックバック状態に変更-- //
 							player->SetKnock();
+
+							// --当たった障害物を消す-- //
 							stage->obstacles.erase(stage->obstacles.begin() + i);
 						}
+
+						// --状態がブースト状態なら-- //
 						else if (player->GetState() == Boost) {
+							// --当たった障害物を消す-- //
 							stage->obstacles.erase(stage->obstacles.begin() + i);
 						}
 					}
@@ -106,27 +149,7 @@ void Collision::Update() {
 		}
 	}
 
-	oldWhiteBox = { {player->GetWhiteObj().pos.x, player->GetWhiteObj().pos.y  + Camera::GetScroll()}, 32, 32 };
-	oldBlackBox = { {player->GetBlackObj().pos.x, player->GetBlackObj().pos.y  + Camera::GetScroll()}, 32, 32 };
+	// --プレイヤーの情報を格納-- //
+	oldWhiteBox = { {player->GetWhiteObj().pos.x, player->GetWhiteObj().pos.y + Camera::GetScroll()}, 32, 32 };
+	oldBlackBox = { {player->GetBlackObj().pos.x, player->GetBlackObj().pos.y + Camera::GetScroll()}, 32, 32 };
 }
-
-// --更新処理-- //
-//void Collision::Update() {
-//	// --プレイヤーの当たり判定フラグがONだったら-- //
-//	if (player->GetCollisionFlag() == true) {
-//		// --黒と白のプレイヤーの座標と半径の情報を格納-- //
-//		Box blackObj = { {player->GetBlackObj().pos.x, player->GetBlackObj().pos.y + Camera::GetScroll()}, player->GetBlackObj().radius, player->GetBlackObj().radius };
-//		Box whiteObj = { {player->GetWhiteObj().pos.x, player->GetWhiteObj().pos.y + Camera::GetScroll()}, player->GetWhiteObj().radius, player->GetWhiteObj().radius };
-//
-//		// --障害物の数分当たり判定をとる-- //
-//		for (int i = 0; i < stage->obstacles.size(); i++) {
-//			// --障害物の座標と半径を格納-- //
-//			Box obstacle = { stage->obstacles[i].GetPos(), stage->obstacles[i].GetRadiusX(), stage->obstacles[i].GetRadiusY() };
-//
-//			// --
-//			if (BoxCollision(whiteObj, obstacle) || BoxCollision(blackObj, obstacle)) {
-//
-//			}
-//		}
-//	}
-//}
